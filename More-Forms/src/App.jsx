317ccb81e4467679625cf2f862c197bebb2e948a
MohammedAbuMukh <mohammedabomokh3@gmail.com>
2025-01-21 15:49:21 +0200

    Add More Forms

---
+++ More-Forms/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import RegisterationForm from './components/RegisterationForm.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <RegisterationForm/>
+    </div>
+  )
+}
+
+export default App
